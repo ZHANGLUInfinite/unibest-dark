fedacb4c86ce3a4a66e740f1630475e5187ad7a5
zhanglu <1280251121@qq.com>
2025-10-13 14:50:01 +0800

    fix(tabbar): 移除 isPageTabbar 函数中的调试日志

--- src/tabbar/store.ts
+++ src/tabbar/store.ts
@@ -25,8 +25,6 @@ if (customTabbarEnable && BULGE_ENABLE) {
 }
 
 export function isPageTabbar(path: string) {
-  console.log('isPageTabbar:', path, tabbarList)
-
   const _path = path.split('?')[0]
   return tabbarList.some(item => item.pagePath === _path)
 }
